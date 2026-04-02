a28dfc2433979d02494f6e17f76404db4caa5195
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-02 18:04:40 +0200

    feat(controller): add function to create subscription

--- src/main/kotlin/com/subtracker/backend/SubscriptionController.kt
+++ src/main/kotlin/com/subtracker/backend/SubscriptionController.kt
@@ -2,6 +2,8 @@ package com.subtracker.backend
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,4 +13,7 @@ class SubscriptionController(private val subscriptionService: SubscriptionServic
 
     @GetMapping("/subscriptions/{id}")
     fun getSubscriptionById(@PathVariable id: Int): Subscription? = subscriptionService.getById(id)
+
+    @PostMapping("/subscriptions")
+    fun addSubscription(@RequestBody subscription: Subscription): Subscription = subscriptionService.add(subscription)
 }
