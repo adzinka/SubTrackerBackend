a2b2b1fa0e7348dceb229d31343875f5245f30bf
Yelizaveta Zhurava <yelizaveta.zhurava@gmail.com>
2026-04-02 18:04:07 +0200

    feat(service): add function to create subscription

--- src/main/kotlin/com/subtracker/backend/SubscriptionService.kt
+++ src/main/kotlin/com/subtracker/backend/SubscriptionService.kt
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class SubscriptionService {
-    private val subscriptions = listOf(
+    private val subscriptions = mutableListOf(
         Subscription(1, "iCloud", 50.0),
         Subscription(2, "iCloud2", 50.0)
     )
@@ -12,4 +12,9 @@ class SubscriptionService {
     fun getAll(): List<Subscription> = subscriptions
 
     fun getById(id: Int): Subscription? = subscriptions.find { it.id == id }
+
+    fun add(subscription: Subscription): Subscription {
+        subscriptions.add(subscription)
+        return subscription
+    }
 }
